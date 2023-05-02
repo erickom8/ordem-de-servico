e5a4f5dc0eb09dfb65c682e76e121bf72380e801
erickom8_ <erick.cirico@gmail.com>
2023-05-02 14:30:54 -0300

    TUAlizando

--- back/src/index.js
+++ back/src/index.js
@@ -28,6 +28,38 @@ app.get('/synapse_os/instituicao', function(req, res){
     })
 })
 
+app.get('/synapse_os/setor/1/instituicao', function(req, res){
+    connection.query('SELECT * FROM setor JOIN instituicao ON setor.id_instituicao = instituicao.id_instituicao WHERE instituicao.id_instituicao = 1 ', function(error, results){
+        if(error)  {
+           throw error;
+        }
+        else {
+            res.send(results)
+        }
+    })
+})
+
+app.get('/synapse_os/setor/2/instituicao', function(req, res){
+    connection.query('SELECT * FROM setor JOIN instituicao ON setor.id_instituicao = instituicao.id_instituicao WHERE instituicao.id_instituicao = 2 ', function(error, results){
+        if(error)  {
+           throw error;
+        }
+        else {
+            res.send(results)
+        }
+    })
+})
+
+app.get('/synapse_os/setor/3/instituicao', function(req, res){
+    connection.query('SELECT * FROM setor JOIN instituicao ON setor.id_instituicao = instituicao.id_instituicao WHERE instituicao.id_instituicao = 3 ', function(error, results){
+        if(error)  {
+           throw error;
+        }
+        else {
+            res.send(results)
+        }
+    })
+})
 
 app.listen(9901, '0.0.0.0', function() {
     console.log('Aplicação rodando na porta 9901')
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -9,10 +9,19 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'synapse_os';
   products:any;
+  SetorFatec:any;
+  SetorSenai: any;
+  SetorColegio: any;
   
   constructor(private http: HttpClient){
-    this.getProducts()
+    this.getProducts() 
+    this.getSetorFatec()
+    this.getSetorSenai()
+    this.getSetorColegio()
+    
   }
+
+
 
   getProducts() {
     this.http.get('http://localhost:9901/synapse_os/instituicao').subscribe((response:any) => {
@@ -20,4 +29,31 @@ export class AppComponent {
       this.products = response
     })
   }
+
+  getSetorFatec() {
+    this.http.get('http://localhost:9901/synapse_os/setor/1/instituicao').subscribe((response:any) => {
+      console.log(response)
+      this.SetorFatec = response 
+    })
+  }
+ 
+
+  getSetorSenai() {
+    this.http.get('http://localhost:9901/synapse_os/setor/2/instituicao').subscribe((response:any) => {
+      console.log(response)
+      this.SetorSenai = response 
+    })
+  }
+
+  getSetorColegio() {
+    this.http.get('http://localhost:9901/synapse_os/setor/3/instituicao').subscribe((response:any) => {
+      console.log(response)
+      this.SetorColegio = response 
+    })
+  }
+ 
+ 
+
+  
+
 }
